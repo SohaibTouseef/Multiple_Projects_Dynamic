cd0bbca1c742875aaa9b99f96e1d580b884e95df
Sohaib Touseef <muhammadsohaib2233344@gmail.com>
2024-10-19 13:32:48 +0500

    Projects

--- Blub/script.ts
+++ Blub/script.ts
@@ -1,4 +1,3 @@
-"use strict";
 // var btn = document.getElementById("toggleBtn");
 // var bulb = document.getElementById("bulb");
 //     btn.addEventListener("click",    function toggleBulb(e) {
@@ -7,21 +6,26 @@
 //      bulb.src="assest/image-bulb-On.jpeg"
 //     btn.textContent = "Turn Off";
 //     } else {
+    
 //       bulb .src ="assest/image-bulb-Off.jpeg";
 //      btn.textContent = "Turn On";
 //     }
 //   } 
 // );
-const btn = document.getElementById("toggleBtn");
-const bulb = document.getElementById("bulb");
+
+const btn = document.getElementById("toggleBtn") as HTMLButtonElement;
+const  bulb = document.getElementById("bulb") as HTMLImageElement;
+
 btn.addEventListener("click", function toggleBulb(e) {
-    // console.log(e); // Optional, for debugging
-    if (btn.innerText.includes("On")) {
-        bulb.src = "assets/image-bulb-Off.jpeg";
-        btn.innerText = "Turn Off";
-    }
-    else {
-        bulb.src = "assets/image-bulb-Off.jpeg";
-        btn.innerText = "Turn On";
-    }
+  // console.log(e); // Optional, for debugging
+  if (btn.innerText.includes("On")) {
+    bulb.src = "assets/image-bulb-on.jpg";
+    btn.innerText = "Turn Off";
+  } else {
+    bulb.src = "assets/image-bulb-Off.jpeg";
+    btn.innerText = "Turn On";
+  }
 });
+
+
+  
